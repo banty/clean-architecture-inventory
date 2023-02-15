71482330702e82bf6cd659454b1938e22ee2f78a
Banty Bahru <bantyder@gmail.com>
2023-02-15 08:06:23 +0100

    Add catalog service API layer

---
+++ CleanArchitectureInventory.Catalog.API/ConfigurationService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CleanArchitectureInventory.Catalog.API.Filters;
+using CleanArchitectureInventory.Catalog.API.Services;
+using CleanArchitectureInventory.Catalog.Application.Common.Abstractions;
+using CleanArchitectureInventory.Catalog.Infrastructure.Persistance;
+
+namespace CleanArchitectureInventory.Catalog.API
+{
+    public static class ConfigurationService
+    {
+        public static IServiceCollection AddAPIService(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddSingleton<ICurrentUserService, CurrentUserService>();
+
+            services.AddHttpContextAccessor();
+
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(typeof(ApiExceptionFilterAttribute));
+                    
+            });
+
+            services.AddHealthChecks()
+                .AddDbContextCheck<ApplicationDbContext>();
+
+           
+            return services;
+        }
+    }
+}
+
---
+++ CleanArchitectureInventory.Catalog.API/Services/CurrentUserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Claims;
+using CleanArchitectureInventory.Catalog.Application.Common.Abstractions;
+
+namespace CleanArchitectureInventory.Catalog.API.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string? UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
+
